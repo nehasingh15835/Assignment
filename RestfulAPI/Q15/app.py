8c732c2bf994648bef4b190cae3cb9ef7feaca74
nehasingh15835 <nehasingh15835@gmail.com>
2025-07-31 22:40:17 +0530

    Update app.py

--- RestfulAPI/Q15/app.py
+++ RestfulAPI/Q15/app.py
@@ -1,1 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# URL with parameter
+@app.route('/user/<username>')
+def show_user(username):
+    return f"<h1>Hello, {username}!</h1>"
+
+# Another example: number
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f"<h1>Viewing Post #{post_id}</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
