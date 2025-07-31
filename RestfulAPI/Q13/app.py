0fd72ceab6a97e72607376cb2c9b22d275299391
nehasingh15835 <nehasingh15835@gmail.com>
2025-07-31 22:33:04 +0530

    Update app.py

--- RestfulAPI/Q13/app.py
+++ RestfulAPI/Q13/app.py
@@ -1,1 +1,17 @@
+from flask import Flask, redirect, url_for, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Redirect to greet with a query parameter (name)
+    return redirect(url_for('greet', name='FlaskUser'))
+
+@app.route('/greet')
+def greet():
+    name = request.args.get('name', 'Guest')
+    return render_template('greet.html', name=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
