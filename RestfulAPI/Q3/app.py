a9e80ebe81c95fa634ffac33dde4b9bb8b4c97de
nehasingh15835 <nehasingh15835@gmail.com>
2025-07-31 21:51:50 +0530

    Update app.py

--- RestfulAPI/Q3/app.py
+++ RestfulAPI/Q3/app.py
@@ -1,1 +1,19 @@
+from flask import Flask, request, render_template
 
+app = Flask(__name__)
+
+# GET method: Just loads the homepage
+@app.route('/')
+def home():
+    return "Welcome to the homepage!"
+
+# GET + POST method: Handles form
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        return f"Hello, {name}!"
+    return render_template("form.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
