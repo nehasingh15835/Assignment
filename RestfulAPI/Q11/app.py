9b090170e0dbfdbf3f8094d5509172049857910e
nehasingh15835 <nehasingh15835@gmail.com>
2025-07-31 22:22:39 +0530

    Update app.py

--- RestfulAPI/Q11/app.py
+++ RestfulAPI/Q11/app.py
@@ -1,1 +1,9 @@
+from flask import Flask
+from myapp.routes import myapp_blueprint
+
+app = Flask(__name__)
+app.register_blueprint(myapp_blueprint)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
