1ebcc3bcf367fccb9eb09d98a3035200b6e79aad
nehasingh15835 <nehasingh15835@gmail.com>
2025-07-31 22:14:04 +0530

    Update app.py

--- RestfulAPI/Q9/app.py
+++ RestfulAPI/Q9/app.py
@@ -1,1 +1,20 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/go-to-contact')
+def go_to_contact():
+    # Redirect to the contact route
+    return redirect(url_for('contact'))
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
