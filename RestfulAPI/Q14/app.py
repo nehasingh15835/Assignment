2cfb402b90cc9f24d8f00570904c9d42466d52fe
nehasingh15835 <nehasingh15835@gmail.com>
2025-07-31 22:38:27 +0530

    Update app.py

--- RestfulAPI/Q14/app.py
+++ RestfulAPI/Q14/app.py
@@ -1,1 +1,21 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Welcome to JSON Demo</h1><p>Go to /api/data to see JSON</p>"
+
+@app.route('/api/data')
+def get_data():
+    data = {
+        "name": "Flask Beginner",
+        "age": 25,
+        "skills": ["Python", "Flask", "APIs"],
+        "is_active": True
+    }
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
