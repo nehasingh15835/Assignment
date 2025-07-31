18b1ddec18de16f4079112baa1cd186a52ed599f
nehasingh15835 <nehasingh15835@gmail.com>
2025-07-31 22:00:40 +0530

    Update app.py

--- RestfulAPI/Q6/app.py
+++ RestfulAPI/Q6/app.py
@@ -1,1 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("form.html")
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    return render_template("result.html", name=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
