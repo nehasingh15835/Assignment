2d0f207e9a06f28e7dcb496fd337d5275478b6da
nehasingh15835 <nehasingh15835@gmail.com>
2025-07-31 22:17:31 +0530

    Update app.py

--- RestfulAPI/Q10/app.py
+++ RestfulAPI/Q10/app.py
@@ -1,1 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# Custom 404 error handler
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
