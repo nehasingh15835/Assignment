dc80f3d1b94b79f2167e43342c4d84d931ba2a33
nehasingh15835 <nehasingh15835@gmail.com>
2025-07-31 22:30:02 +0530

    Update app.py

--- RestfulAPI/Q12/app.py
+++ RestfulAPI/Q12/app.py
@@ -1,1 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Custom filter: reverse a string
+def reverse_string(s):
+    return s[::-1]
+
+# Register filter with Jinja
+app.jinja_env.filters['reverse'] = reverse_string
+
+@app.route('/')
+def home():
+    name = "Flask Beginner"
+    return render_template("home.html", name=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
